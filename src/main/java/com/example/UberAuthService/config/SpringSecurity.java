b9e9fb996b4b04e3a6c3629bd66fee99eab3922c
Sajid Mallick <anonymouscoder7782@gmail.com>
2026-05-23 12:22:12 +0530

    Some configuration changes

--- src/main/java/com/example/UberAuthService/config/SpringSecurity.java
+++ src/main/java/com/example/UberAuthService/config/SpringSecurity.java
@@ -2,10 +2,19 @@ package com.example.UberAuthService.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurity {
 
     @Bean
@@ -13,5 +22,24 @@ public class SpringSecurity {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(
+                        AbstractHttpConfigurer::disable
+                )
+                .cors(
+                        AbstractHttpConfigurer::disable
+                )
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/v3/api-docs/**",
+                                        "api/v1/auth/**"
+                                ).permitAll()
+                )
+                .build();
+    }
 }
-
--- src/main/java/com/example/UberAuthService/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/UberAuthService/exception/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import com.example.UberAuthService.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
