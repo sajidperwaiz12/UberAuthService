b749f4742119ae70f8d9a57b19fd6aa130d01145
Sajid Mallick <anonymouscoder7782@gmail.com>
2026-05-23 13:53:04 +0530

    Added custom user details

---
+++ src/main/java/com/example/UberAuthService/helpers/AuthUserDetails.java
@@ -0,0 +1,57 @@
+package com.example.UberAuthService.helpers;
+
+import com.example.UberAuthService.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AuthUserDetails implements UserDetails {
+    private final String username; // email
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public AuthUserDetails(User user) {
+        this.username = user.getEmail();
+        this.password = user.getPassword();
+        this.authorities = List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
+
---
+++ src/main/java/com/example/UberAuthService/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.UberAuthService.services;
+
+import com.example.UberAuthService.entities.User;
+import com.example.UberAuthService.helpers.AuthUserDetails;
+import com.example.UberAuthService.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(()-> {
+            return new UsernameNotFoundException("User not found");
+        });
+        return new AuthUserDetails(user);
+    }
+}
+
